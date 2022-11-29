818944017fc552b22637bbee30e6639bd2056ddf
wojciech-turek <contact@wojciechturek.com>
2022-11-29 15:10:55 +0100

    Basic wallet connection and styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,41 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+
+const chains = [chain.mainnet];
+
+if (!process.env.NEXT_PUBLIC_PROJECT_ID) {
+  throw new Error("You need to provide NEXT_PUBLIC_PROJECT_ID env variable");
+}
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: projectId }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({
+    appName: "Fontend Assessment Biconomy",
+    chains,
+  }),
+  provider,
+});
+
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <Component {...pageProps} />
+      </WagmiConfig>
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
+  );
 }
